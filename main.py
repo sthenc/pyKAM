0ba4b9b87370b3948ec5523ef9d362e98915f825
Stjepan Henc <sthenc@gmail.com>
2014-05-04 12:59:01 +0200

    popravak repozitorija

--- main.py
+++ main.py
@@ -17,9 +17,37 @@
 #
 ################################################################################
 
-import audioread
 
-indf = audioread.audio_open('../../data/wiener_blut1.wav')
 
-print(indf.channels, indf.samplerate, indf.duration)
+#import audioread
+#indf = audioread.audio_open('../../data/trololo.mp4')
+#ar = [x for x in indf];
+#print(indf.channels, indf.samplerate, indf.duration)
+#print(ar[0])
+#indf.close()
+
+import numpy as np
+import scipy.io.wavfile as wav
+from scipy.interpolate import interp1d
+
+
+sr, y = wav.read("../../data/stereo-blauen1.wav")
+
+ly = y.T[0]
+ry = y.T[1]
+
+
+x = np.linspace(0, 10000, len(y))
+lf = interp1d(x, ly)
+rf = interp1d(x, ry)
+
+print("pocetak racunanja")
+sr2 = 96000
+ly2 = [lf(t) for t in np.linspace(0, 10000, len(y) * sr2/sr)]
+ry2 = [rf(t) for t in np.linspace(0, 10000, len(y) * sr2/sr)]
+
+
+y2 = array([ly2,ry2]).T
+
+wav.write("./stereo-resampled.wav", sr2, y2)
 
